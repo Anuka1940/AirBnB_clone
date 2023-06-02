2c4bf1eb6be779f3a3e94dd1dff197bb9f0af736
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-06-01 17:03:44 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,18 @@ from datetime import datetime
 
 class BaseModel:
     '''Define all attributes and methods'''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Serialization or Deserialization'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(kwargs[key])
+                if key != '__class__':
+                    setattr(self, key, value)
 
 
     def __str__(self):
