b2e39aeca6fa51525a741d4a2796a95b446f2566
Abdul Lateef Anuka <anukalateef@gmail.com>
2024-03-22 16:52:22 +0100

    BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -2,43 +2,43 @@
 '''A base Model for other project models'''
 import uuid
 from datetime import datetime
-from models.engine.file_storage import FileStorage
-from models import storage
 
 
 class BaseModel:
-    '''Define all attributes and methods'''
+    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        '''Serialization or Deserialization'''
+        """Instatntiates a new model"""
         if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.fromisoformat(kwargs[key])
-                if key != '__class__':
-                    setattr(self, key, value)
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
-        '''Override default __str__ method'''
-        fmt = '[{}] ({}) {}'
-        return (fmt.format(type(self).__name__, self.id, self.__dict__))
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        '''Update updated_at with current datetime'''
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        '''return a dict containing all keys/values of "__dict__"'''
-        new_dict = {}
-        new_dict["__class__"] = self.__class__.__name__
-        for key, value in self.__dict__.items():
-            if key in ['created_at', 'updated_at']:
-                new_dict[key] = value.isoformat()
-            else:
-                new_dict[key] = value
-        return new_dict
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
